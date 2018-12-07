cca008d3c75dc48f2572223e6b8801c840b539cc
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-07 08:42:39 -0800

    Add required gems and database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,14 @@
-require 'rspec'
-require 'pg'
-require 'pry'
-require 'project'
-require 'volunteer'
-
+require "volunteer"
+require "project"
+require "rspec"
+require "pry"
+require "pg"
 
 DB = PG.connect({:dbname => 'volunteer_tracker_test'})
 
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec("DELETE FROM projects *;")
-    DB.exec("DELETE FROM volunteers *;")
+    DB.exec('DELETE FROM volunteers *;')
+    DB.exec('DELETE FROM projects *;')
   end
 end
