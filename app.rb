769c7adab88fe4b57a0262de30ef6481078e70f1
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-07 11:43:58 -0800

    Add volunteer list to project page

--- app.rb
+++ app.rb
@@ -6,10 +6,11 @@ require('./lib/volunteer')
 require('pg')
 require('pry')
 
-DB = PG.connect({dbname: 'volunteer_tracker'})
+# DB = PG.connect({dbname: 'volunteer_tracker'})
 
 get '/' do
   @projects = Project.all
+  @volunteers = Volunteer.all
   erb :index
 end
 
@@ -23,21 +24,16 @@ end
 get '/projects/:id' do
   id = params[:id].to_i
   @project = Project.find(id)
+  @volunteers = @project.volunteers
   erb :project
 end
 
-get '/projects/:id/edit' do
-  id = params[:id].to_i
-  @project = Project.find(id)
-  erb :project_edit
-end
-
-patch '/projects/:id/update' do
+patch '/projects/:id/edit' do
   id = params[:id].to_i
   @project = Project.find(id)
   new_title = params['title']
   @project.update({title: new_title, id: id})
-  redirect('/')
+  redirect("/projects/#{params["id"]}")
 end
 
 patch '/projects/:id/delete' do
@@ -45,4 +41,29 @@ patch '/projects/:id/delete' do
   project = Project.find(id)
   project.delete
   redirect '/'
+end
+
+post '/volunteers/add' do
+  name = params['name']
+  project_id = params['project_id']
+  volunteer = Volunteer.new({name: name, project_id: project_id, id: nil})
+  volunteer.save
+  redirect '/'
+end
+
+get '/volunteers/:id' do
+  id = params[:id].to_i
+  @projects = Project.all
+  @volunteer = Volunteer.find(id)
+  @project = Project.find(@volunteer.project_id)
+  erb :volunteer
+end
+
+patch '/volunteers/:id/edit' do
+  id = params[:id].to_i
+  @volunteer = Volunteer.find(id)
+  new_name = params['name']
+  new_project_id = params['project_id']
+  @Volunteer.update({name: new_name, project_id: new_project_id, id: id})
+  redirect("/volunteers/#{params["id"]}")
 end
