58f86bd9d788f25214172e64edc3147d5bd352c1
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-07 09:40:08 -0800

    Pass Project spec 9; add delete method

--- lib/project.rb
+++ lib/project.rb
@@ -31,6 +31,10 @@ class Project
     DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
   end
 
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id}")
+  end
+
   def volunteers
     volunteers_db = DB.exec("SELECT * FROM volunteers WHERE project_id = #{id};")
     volunteers = []
