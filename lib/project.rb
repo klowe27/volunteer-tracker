7d3e62adbc8221c950b0158c2e2a3143890143bf
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-07 08:53:31 -0800

    Pass Project spec 2; add save method

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,12 @@
 class Project
   attr_reader :title, :id
-  
+
   def initialize(attributes)
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
+  end
+
+  def save
+    @id = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;").first["id"].to_i
   end
 end
