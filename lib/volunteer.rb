e2613241f8379390fd804023a0a3f2c4bbc7d9f0
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-07 09:19:20 -0800

    Pass Volunteer spec 4; add all and save methods

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,6 +7,22 @@ class Volunteer
     @id = attributes.fetch(:id)
   end
 
+  def self.all
+    volunteers_db = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    volunteers_db.each do |volunteer|
+      name = volunteer["name"]
+      project_id = volunteer["project_id"].to_i
+      id = volunteer["id"].to_i
+      volunteers.push(Volunteer.new({name: name, project_id: project_id, id: id}))
+    end
+    volunteers
+  end
+
+  def save
+    @id = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;").first["id"].to_i
+  end
+
   def ==(another_object)
     self.id.==(another_object.id).&self.name.==(another_object.name)
   end
