41ab229f4863fb3d44ce2515dde5c887b23d211f
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-07 10:26:04 -0800

    Pass integration spec 2; add project page and update form

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require('./lib/volunteer')
 require('pg')
 require('pry')
 
-DB = PG.connect({dbname: 'volunteer_tracker'})
+# DB = PG.connect({dbname: 'volunteer_tracker'})
 
 get '/' do
   @projects = Project.all
@@ -18,4 +18,25 @@ post '/projects/add' do
   project = Project.new({title: title, id: nil})
   project.save
   redirect '/'
+end
+
+get '/projects/:id' do
+  id = params[:id].to_i
+  @project = Project.find(id)
+  erb :project
+end
+
+get '/projects/:id/edit' do
+  id = params[:id].to_i
+  @project = Project.find(id)
+  erb :project_edit
+end
+
+patch '/projects/:id/update' do
+  id = params[:id].to_i
+  @project = Project.find(id)
+  new_title = params['title']
+  @project.update({title: new_title, id: id})
+  redirect('/')
+  # redirect('/projects/#{id}')
 end
