73adf89af564e8b7563f8fe6f528a5b3bcb5d674
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-07 09:01:42 -0800

    Pass Project spec 4; add all method

--- lib/project.rb
+++ lib/project.rb
@@ -6,6 +6,17 @@ class Project
     @id = attributes.fetch(:id)
   end
 
+  def self.all
+    projects_db = DB.exec("SELECT * FROM projects;")
+    projects = []
+    projects_db.each do |project|
+      title = project["title"]
+      id = project["id"].to_i
+      projects.push(Project.new({title: title, id: id}))
+    end
+    projects
+  end
+
   def save
     @id = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;").first["id"].to_i
   end
