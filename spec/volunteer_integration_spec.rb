3d62f9f9f33de959729114c939bf57cad1f72578
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-07 17:04:02 -0800

    Add additional integration tests

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -6,10 +6,6 @@ require('spec_helper')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# Your project should be set up so that a volunteer can only be created if a project already exists. (This makes it easier to assign the one to many relationship in Sinatra.) Focus on getting one integration spec passing at a time.
-
-# The user should be able to visit the home page and fill out a form to add a new project. When that project is created, the application should direct them back to the homepage.
-
 describe 'the project creation path', {:type => :feature} do
   it 'takes the user to the homepage where they can create a project' do
     visit '/'
@@ -19,7 +15,14 @@ describe 'the project creation path', {:type => :feature} do
   end
 end
 
-# A user should be able to click on a project to see its detail. The detail page includes a form where the project can be updated. When the form is submitted, the user can be directed to either the home page or that project's detail page. (The test will work for either.)
+describe 'the project detail page path', {:type => :feature} do
+  it 'shows a project detail page' do
+    test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    test_project.save
+    visit "/projects/#{test_project.id}"
+    expect(page).to have_content('Teaching Kids to Code')
+  end
+end
 
 describe 'the project update path', {:type => :feature} do
   it 'allows a user to change the name of the project' do
@@ -33,8 +36,6 @@ describe 'the project update path', {:type => :feature} do
   end
 end
 
-# A user should be able to nagivate to a project's detail page and delete the project. The user will then be directed to the index page. The project should no longer be on the list of projects.
-
 describe 'the project delete path', {:type => :feature} do
   it 'allows a user to delete a project' do
     test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
@@ -45,8 +46,6 @@ describe 'the project delete path', {:type => :feature} do
     expect(page).not_to have_content("Teaching Kids to Code")
   end
 end
-
-# The user should be able to click on a project detail page and see a list of all volunteers working on that project. The user should be able to click on a volunteer to see the volunteer's detail page.
 
 describe 'the volunteer detail page path', {:type => :feature} do
   it 'shows a volunteer detail page' do
@@ -61,5 +60,19 @@ describe 'the volunteer detail page path', {:type => :feature} do
     fill_in('hours', :with => 8)
     click_button('Update Volunteer')
     expect(page).to have_content('Jane')
+  end
+end
+
+describe 'the volunteer delete path', {:type => :feature} do
+  it 'allows a user to delete a volunteer' do
+    test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    test_project.save
+    project_id = test_project.id
+    test_volunteer = Volunteer.new({:name => 'Jasmine', :project_id => project_id, :hours => 4, :id => nil})
+    test_volunteer.save
+    id = test_volunteer.id
+    visit "/volunteers/#{id}"
+    click_button('Delete Volunteer')
+    expect(page).not_to have_content('Jasmine')
   end
 end
